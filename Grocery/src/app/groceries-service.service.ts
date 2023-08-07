340f6b4206b4a5835e4133861c8190c06d1aceeb
kevinsherrell <k.sherrell.1028@gmail.com>
2023-08-07 16:36:13 -0500

    refactored all code to latest version of ionic and capacitor

---
+++ Grocery/src/app/groceries-service.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {Item} from "../Types";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroceriesServiceService {
+
+  items: Item[] = [];
+
+  constructor() {
+    console.log("Hello GroceriesServiceProvider Provider");
+  }
+
+  getItems(){
+    return this.items;
+  }
+
+  removeItem(index: number){
+    this.items.splice(index, 1)
+  }
+
+  addItem(item: Item){
+    this.items.push(item);
+  }
+
+  editItem(item: Item, index: number) : void{
+    this.items[index] = item;
+  }
+}
